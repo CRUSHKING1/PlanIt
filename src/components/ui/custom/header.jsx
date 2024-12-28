a5568a411283e62d522207dfd5fb263408133fa9
CRUSHKING1 <punithpuni.leo@gmail.com>
2024-12-28 22:44:57 +0530

    Modify Header component with sign-in and logout functionality
    - Updated the Header component to include sign-in and logout functionality.
    - Improved user authentication flow to support these new features.
    - Enhanced UI elements for better usability.
    
    Note: The project is still under development.

--- src/components/ui/custom/header.jsx
+++ src/components/ui/custom/header.jsx
@@ -1,14 +1,92 @@
-import React from 'react'
-import { Button } from '../button'
+import React, { useEffect,useState } from "react";
+import { Button } from "../button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { googleLogout } from "@react-oauth/google";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { FcGoogle } from "react-icons/fc";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 function Header() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [openDailog, setOpenDailog] = useState(false);
+  useEffect(() => {
+    console.log(user);
+  }, []);
+  
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => GetUserProfile(codeResponse),
+    onError: (error) => console.log(error),
+  });
+  async function GetUserProfile(tokenInfo) {
+    await axios
+      .get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?acess_token=${tokenInfo?.access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokenInfo?.access_token}`,
+            Accept: "Application/json",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem("user", JSON.stringify(response.data));
+        setOpenDailog(false);
+         window.location.reload();
+      });
+  }
   return (
-    <div className='p-3 shadow-sm flex justify-between items-center px-5'>
-        <img src="/logo.svg" alt="logo" />
-        <div>
-            <Button>Sign in</Button>
-        </div>
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
+      <img src="/logo.svg" alt="logo" />
+      <div>{user ? <div className="flex  items-center gap-3">
+        <Button variant="outline" className="rounded-full">My trip</Button>
+
+       
+        <Popover>
+  <PopoverTrigger className="bg-white rounded-full"><img src={user?.picture} alt="userimage" className="h-[35px] w-[35px] rounded-full"/></PopoverTrigger>
+  <PopoverContent><h2 className="cursor-pointer" onClick={()=>{
+    googleLogout();
+    localStorage.clear();
+   window.location.reload();
+  }}>Logout</h2></PopoverContent>
+</Popover>
+
+      </div> : <Button onClick={()=>setOpenDailog(true)}>Sign in</Button>
+      
+      }</div>
+        <Dialog open={openDailog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              <img src="/logo.svg" alt="logo" />
+              <h2 className="font-bold text-lg mt-7">Sign In With Google</h2>
+              <p>Sign in to the App with Google Authentiction Securely </p>
+              <Button
+                onClick={login}
+                className="w-full mt-5 flex gap-4 items-center"
+              >
+                <FcGoogle className="h-7 w-7" />
+                Sign in with Google
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+    
+  );
 }
 
-export default Header
+export default Header;
